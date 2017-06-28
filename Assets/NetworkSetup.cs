b3b5e39fb9ab856cdc81df14c6c43a1a663cdbd8
Ronja <axoila@gmx.de>
2017-06-29 00:35:18 +0200

    fps

---
+++ Assets/NetworkSetup.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkSetup : NetworkBehaviour {
+    [SerializeField] Behaviour[] playerOnly;
+    // Use this for initialization
+    void Start () {
+		foreach(Behaviour playerOnlyBehaviour in playerOnly){
+            playerOnlyBehaviour.enabled = isLocalPlayer;
+        }
+	}
+}
---
+++ Assets/Shooting.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Shooting : NetworkBehaviour {
+
+    Camera cam;
+
+	[SyncVar]int health = 5;
+
+    // Use this for initialization
+    void Awake () {
+        cam = GetComponentInChildren<Camera>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Fire1")){
+            CmdShoot();
+        }
+	}
+
+	[Command]
+    public void CmdShoot()
+    {
+		RaycastHit hit;
+        Ray ray = cam.ViewportPointToRay(cam.rect.size * 0.5f);
+        if (Physics.Raycast(ray, out hit, 15))
+        {
+            RpcShoot(ray.origin, hit.point);
+            Shooting enemyShot = hit.collider.GetComponent<Shooting>();
+            if (enemyShot && enemyShot != this)
+            {
+                enemyShot.CmdGetShot();
+            }
+        }
+        else
+        {
+            RpcShoot(ray.origin, ray.direction * 15);
+        }
+    }
+
+	[ClientRpc]
+	public void RpcShoot(Vector3 start, Vector3 end){
+		Debug.DrawLine(start, end, Color.red, 1, true);
+    }
+	[Command]
+    public void CmdGetShot(){
+        health--;
+        RpcGetShot();
+    }
+	[ClientRpc]
+	public void RpcGetShot(){
+        print("I got shot");
+    }
+}
